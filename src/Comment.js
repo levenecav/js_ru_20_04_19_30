54b8168593e57e77563866c4a121e64691185725
levenecav-r <a.levenec@gmail.com>
2017-04-27 11:23:56 +0300

    Comment as Functional Component

--- src/Comment.js
+++ src/Comment.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react'
 
 //Это можно сделать Functional Component, по возможности используй их
-export default class Comment extends Component {
-    
-    render() {
-        const {comment} = this.props;
-        return (
-            <div className="comment">
-                <h5>{comment.user}</h5>
-                <div>{comment.text}</div>
-            </div>
-        )
-    }
+export default function Comment(props) {
+    const {comment} = props;
+    return (
+        <div className="comment">
+            <h5>{comment.user}</h5>
+            <div>{comment.text}</div>
+        </div>
+    )
 }
