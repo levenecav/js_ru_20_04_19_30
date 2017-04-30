0850d4293948119b8962f2654e290a24c1545512
a.levenec <a.levenec@gmail.com>
2017-04-30 09:43:55 +0300

    HT2.2 ready

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react'
 import Article from './Article'
+import toggleArticle from '../decorators/toggleArticle'
 import PropTypes from 'prop-types'
 
 class ArticleList extends Component {
 
-    state = {
-        openArticleId: null
-    }
-
     render() {
         const elements = this.props.articles.map(article => <li key={article.id}>
             <Article article={article}
-                     isOpen={article.id == this.state.openArticleId}
-                     toggleOpen={this.toggleArticle(article.id)}/>
+                     isOpen={article.id == this.props.openArticleId}
+                     toggleOpen={this.props.toggleArticle(article.id)}/>
         </li>)
         return (
             <ul>
@@ -20,16 +17,13 @@ class ArticleList extends Component {
             </ul>
         )
     }
-
-    toggleArticle = id => ev => {
-        this.setState({
-            openArticleId: id
-        })
-    }
 }
 
 ArticleList.propTypes = {
-    articles: PropTypes.array
+    articles: PropTypes.array,
+    //from toggleOpen decorator
+    openArticleId: PropTypes.string,
+    toggleArticle: PropTypes.func
 }
 
-export default ArticleList
+export default toggleArticle(ArticleList);
