fe30a2037fe1c25b97722f7d1c7b37706789f1c9
Roman <kaboom.ua@gmail.com>
2017-05-18 11:16:04 +0300

    Update selection.js

--- src/reducer/selection.js
+++ src/reducer/selection.js
@@ -1,5 +1,5 @@
 import {SELECT_ARTICLE} from '../constants'
-
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (selection = [], action) => {
     const {type, payload} = action;
     return type == SELECT_ARTICLE ? payload.selection : selection;
