e05f2439496794fbb1ff29a552d48cd652ed5379
Roman <kaboom.ua@gmail.com>
2017-05-22 14:59:37 +0300

    Update doerId.js

--- src/middlewares/doerId.js
+++ src/middlewares/doerId.js
@@ -1,5 +1,6 @@
 export default store => next => action => {
 	// console.log('--action.type--', action.type)
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - не лучшее решение
     if(action.type == 'ADD_COMMENT') action.payload.id = Math.random().toString(36).substr(2, 9);
     next(action);
 }
